fb322de73d9e1af30996ee0536ad29cbca33c416
Serdar GOKCEN <gokcen.serdar@gmail.com>
2010-11-28 05:55:42 -0800

    many more methods added

--- selenium-lisp-client.lisp
+++ selenium-lisp-client.lisp
@@ -82,10 +82,6 @@
 				,@args)))
     (subseq response 3)))
 
-;;not yet implemented
-(defmethod get-string-array (selenium verb &rest args)
-  nil)
-
 (defmethod selenium-start(selenium)
   (let ((response (get-string  selenium
 			       "getNewBrowserSession"
@@ -114,8 +110,193 @@
 (defmethod selenium-click (selenium locator)
   (get-request selenium "click" locator))
 
+(defmethod selenium-double-click (selenium locator)
+  (get-request selenium "doubleClick" locator))
+
+(defmethod selenium-context-menu (selenium locator)
+  (get-request selenium "contextMenu" locator))
+
+(defmethod selenium-click-at (selenium locator coord)
+  (get-request selenium "clickAt" locator coord))
+
+(defmethod selenium-double-click-at (selenium locator coord)
+  (get-request selenium "doubleClickAt" locator coord))
+
+(defmethod selenium-context-menu-at (selenium locator coord)
+  (get-request selenium "contextMenuAt" locator coord))
+
+(defmethod selenium-fire-event (selenium locator event-name)
+  (get-request selenium "fireEvent" locator event-name))
+
+(defmethod selenium-focus (selenium locator)
+  (get-request selenium "focus" locator))
+
+(defmethod selenium-key-press (selenium locator key-sequence)
+  (get-request selenium "keyPress" locator key-sequence))
+
+(defmethod selenium-shift-key-down (selenium)
+  (get-request selenium "shiftKeyDown"))
+
+(defmethod selenium-shift-key-up (selenium)
+  (get-request selenium "shiftKeyUp"))
+
+(defmethod selenium-meta-key-down (selenium)
+  (get-request selenium "metaKeyDown"))
+
+(defmethod selenium-meta-key-up (selenium)
+  (get-request selenium "metaKeyUp"))
+
+(defmethod selenium-alt-key-down (selenium)
+  (get-request selenium "altKeyDown"))
+
+(defmethod selenium-alt-key-up (selenium)
+  (get-request selenium "altKeyUp"))
+
+(defmethod selenium-control-key-down (selenium)
+  (get-request selenium "controlKeyDown"))
+
+(defmethod selenium-control-key-up (selenium)
+  (get-request selenium "controlKeyUp"))
+
+(defmethod selenium-key-down (selenium locator key-sequence)
+  (get-request selenium "keyDown" locator key-sequence))
+
+(defmethod selenium-key-up (selenium locator key-sequence)
+  (get-request selenium "keyUp" locator key-sequence))
+
+(defmethod selenium-mouse-over (selenium locator)
+  (get-request selenium "mouseOver" locator))
+
+(defmethod selenium-mouse-out (selenium locator)
+  (get-request selenium "mouseOut" locator))
+
+(defmethod selenium-mouse-down (selenium locator)
+  (get-request selenium "mouseDown" locator))
+
+(defmethod selenium-mouse-down-right (selenium locator)
+  (get-request selenium "mouseDownRight" locator))
+
+(defmethod selenium-mouse-down-at (selenium locator coord)
+  (get-request selenium "mouseDownAt" locator coord))
+
+(defmethod selenium-mouse-down-right-at (selenium locator coord)
+  (get-request selenium "mouseDownRightAt" locator coord))
+
+(defmethod selenium-mouse-up (selenium locator)
+  (get-request selenium "mouseUp" locator))
+
+(defmethod selenium-mouse-up-right (selenium locator)
+  (get-request selenium "mouseUpRight" locator))
+
+(defmethod selenium-mouse-up-at (selenium locator coord)
+  (get-request selenium "mouseUpAt" locator coord))
+
+(defmethod selenium-mouse-up-right-at (selenium locator coord)
+  (get-request selenium "mouseUpRightAt" locator coord))
+
+(defmethod selenium-mouse-move (selenium locator)
+  (get-request selenium "mouseMove" locator))
+
+(defmethod selenium-mouse-move-at (selenium locator coord)
+  (get-request selenium "mouseMoveAt" locator coord))
+
+(defmethod selenium-type-keys (selenium locator value)
+  (get-request selenium "typeKeys" locator value))
+
+(defmethod selenium-set-speed (selenium value)
+  (get-request selenium "setSpeed" value))
+
+(defmethod selenium-get-speed (selenium)
+  (get-request selenium "getSpeed"))
+
+(defmethod selenium-check (selenium locator)
+  (get-request selenium "check" locator))
+
+(defmethod selenium-uncheck (selenium locator)
+  (get-request selenium "uncheck" locator))
+
+(defmethod selenium-select (selenium select-locator option-locator)
+  (get-request selenium "select" select-locator option-locator))
+
+(defmethod selenium-add-selection (selenium locator option-locator)
+  (get-request selenium "addSelection" locator option-locator))
+
+(defmethod selenium-remove-selection (selenium locator option-locator)
+  (get-request selenium "removeSelection" locator option-locator))
+
+(defmethod selenium-remove-all-selections (selenium locator)
+  (get-request selenium "removeAllSelections" locator))
+
+(defmethod selenium-submit (selenium form-locator)
+  (get-request selenium "submit" form-locator))
+
+(defmethod selenium-open-window (selenium url window-id)
+  (get-request selenium "openWindow" url window-id))
+
+(defmethod selenium-select-window (selenium window-id)
+  (get-request selenium "selectWindow" window-id))
+
+(defmethod selenium-select-pop-up (selenium window-id)
+  (get-request selenium "selectPopUp" window-id))
+
+(defmethod selenium-deselect-pop-up (selenium)
+  (get-request selenium "deselectPopUp"))
+
+(defmethod selenium-select-frame (selenium locator)
+  (get-request selenium "selectFrame" locator))
+
+(defmethod selenium-wait-for-pop-up (selenium window-id timeout)
+  (get-request selenium "waitForPopUp" window-id timeout))
+
+(defmethod selenium-choose-cancel-on-next-confirmation (selenium)
+  (get-request selenium "chooseCancelOnNextConfirmation"))
+
+(defmethod selenium-choose-ok-on-next-confirmation (selenium)
+  (get-request selenium "chooseOkOnNextConfirmation"))
+
+(defmethod selenium-answer-on-next-prompt (selenium answer)
+  (get-request selenium "answerOnNextPrompt" answer))
+
+(defmethod selenium-go-back (selenium)
+  (get-request selenium "goBack"))
+
+(defmethod selenium-refresh (selenium)
+  (get-request selenium "refresh"))
+
+(defmethod selenium-close (selenium)
+  (get-request selenium "close"))
+
+(defmethod selenium-alert-present? (selenium)
+  (get-string selenium "isAlertPresent"))
+
+(defmethod selenium-prompt-present? (selenium)
+  (get-string selenium "isPromptPresent"))
+
+(defmethod selenium-confirmation-present? (selenium)
+  (get-string selenium "isConfirmationPresent"))
+
+(defmethod selenium-get-alert (selenium)
+  (get-string selenium "getAlert"))
+
+(defmethod selenium-get-confirmation (selenium)
+  (get-string selenium "getConfirmation"))
+
+(defmethod selenium-get-prompt (selenium)
+  (get-string selenium "getPrompt"))
+
+(defmethod selenium-get-location (selenium)
+  (get-string selenium "getLocation"))
+
 (defmethod selenium-get-title (selenium)
-  (get-request selenium "getTitle"))
+  (get-string selenium "getTitle"))
+
+(defmethod selenium-get-body-text (selenium)
+  (get-string selenium "getBodyText"))
+
+(defmethod selenium-get-value (selenium locator)
+  (get-string selenium "getValue" locator))
+
+
 
 
 (defun start-test ()
@@ -136,5 +317,4 @@
     (selenium-click selenium1 "btnG")
     (selenium-wait-for-page-to-load selenium1 5000)
     (format t "~A~%" (selenium-get-title selenium1))
-    (selenium-stop selenium1)
-    ))
+    (selenium-stop selenium1)))
